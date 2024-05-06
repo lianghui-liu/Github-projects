00a3e17e1ac0461714f51440902cbb703970a169
lianghui-liu <2430220602@qq.com>
2024-05-06 15:09:58 +0800

    Initialization and Add perovskite related codes

---
+++ Perovskite-I-V-curve.bat
@@ -0,0 +1,9 @@
+jupyter nbconvert --to python "C:\Users\24302\OneDrive - stu.xjtu.edu.cn\×ÀÃæ\Perovskite-I-V-curve.ipynb"
+
+pyinstaller -F "C:\Users\24302\OneDrive - stu.xjtu.edu.cn\×ÀÃæ\Perovskite-I-V-curve.py"
+
+cd "C:\Users\24302\OneDrive - stu.xjtu.edu.cn\×ÀÃæ\dist" 
+
+start Perovskite-I-V-curve.exe
+
+pause
